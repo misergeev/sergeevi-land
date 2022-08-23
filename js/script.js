e57bfd1b013ffd16d11d10c5f34f4c3ffd94eb90
Michail <mialsergeev@gmail.com>
2022-08-24 00:33:56 +0500

    New commit 2022.08.24

--- js/script.js
+++ js/script.js
@@ -32,8 +32,10 @@ $(document).ready(function() {
 
   function gardensShowItem() {
     $('.gardens__wrapper .gardens__item').each(function() {
-      if ($(window).scrollTop() >= $(this).offset().top - ($(window).height() - 250)) {
+      if ($(window).scrollTop() >= $(this).offset().top - ($(window).height() - 100)) {
         $(this).addClass('show');
+      } else {
+        $(this).removeClass('show');
       }
     });
   }
@@ -52,5 +54,65 @@ $(document).ready(function() {
     $(this).parent().fadeOut(300);
     $('.gardens').css('margin-bottom', 116);
   });
+
+  $(document).on('click', '.js-garden-tab', function() {
+    var $this = $(this),
+        tabIndex = $this.index();
+    
+    $('.js-garden-tab').removeClass('active');
+    $this.addClass('active');
+
+    $('.gardensDetail__content').css({
+      'opacity': 0,
+      'height': '100vh',
+    });
+
+    setTimeout(function() {
+      var contenTab = $('.gardensDetail__content-item').eq(tabIndex);
+      $('.gardensDetail__content-item').not(contenTab).removeClass('show');
+      $('.gardensDetail__content-item').eq(tabIndex).addClass('show');
+
+      if ($this.attr('data-tab') == 'gallery') {
+        $('.gardensDetail__gallery').isotope({
+          itemSelector: 'img',
+          layoutMode: 'masonry',
+          percentPosition: true,
+          masonry: {
+            gutter: 10,
+            horizontalOrder: true,
+          }
+        });
+      }
+    }, 300);
+
+    setTimeout(function() {
+      $('.gardensDetail__content').css({
+        'opacity': 1,
+        'height': 'auto'
+      });
+    },700);
+  });
+
+  $(document).on('click', '.js-garden-gallery', function() {
+    $('body, html').animate({
+      scrollTop: $('.gardensDetail__tabs').offset().top - 150
+    }, 1000);
+
+    setTimeout(function() {
+      $('.js-garden-tab[data-tab="gallery"]').trigger('click');
+    }, 1000);
+
+  });
+
+  $('.gardensDetail__gallery').isotope({
+    itemSelector: 'img',
+    layoutMode: 'masonry',
+    percentPosition: true,
+    masonry: {
+      gutter: 10,
+      horizontalOrder: true,
+    }
+  });
+
 
 });
